2dadfb059eff3f3de9ec53a461a227c0def0fbc4
Benjamin Cooke <benjaminrcooke@gmail.com>
2024-01-16 13:01:03 -0500

    Added Media fucntion keys and default Master size adjustment.

--- dwm/config.h
+++ dwm/config.h
@@ -1,5 +1,7 @@
 /* See LICENSE file for copyright and license details. */
 
+#include <X11/XF86keysym.h>
+
 /* appearance */
 static const unsigned int borderpx       = 0;   /* border pixel of windows */
 static const int corner_radius           = 10;
@@ -7,8 +9,7 @@ static const unsigned int snap           = 32;  /* snap pixel */
 static const unsigned int gappih         = 20;  /* horiz inner gap between windows */
 static const unsigned int gappiv         = 10;  /* vert inner gap between windows */
 static const unsigned int gappoh         = 10;  /* horiz outer gap between windows and screen edge */
-static const unsigned int gappov         = 30;  /* vert outer gap between windows and screen edge */
-static const int smartgaps_fact          = 1;   /* gap factor when there is only one client; 0 = no gaps, 3 = 3x outer gaps */
+static const unsigned int gappov         = 30;  /* vert outer gap between windows and screen edge */ static const int smartgaps_fact          = 1;   /* gap factor when there is only one client; 0 = no gaps, 3 = 3x outer gaps */
 static const int showbar                 = 1;   /* 0 means no bar */
 static const int topbar                  = 1;   /* 0 means bottom bar */
 /* Status is to be shown on: -1 (all monitors), 0 (a specific monitor by index), 'A' (active monitor) */
@@ -26,7 +27,7 @@ static const int ulineall = 0;                  /* 1 to show underline on all ta
 static int tagindicatortype              = INDICATOR_TOP_LEFT_SQUARE;
 static int tiledindicatortype            = INDICATOR_NONE;
 static int floatindicatortype            = INDICATOR_TOP_LEFT_SQUARE;
-static const char *fonts[]               = { "Hack:size=14", "ttf-font-nerd:size=20" };
+static const char *fonts[]               = { "Hack:size=16", "ttf-font-nerd:size=20" };
 static const char dmenufont[]            = { "Hack:size=14" };
 
 static char c000000[]                    = "#000000"; // placeholder value
@@ -46,7 +47,7 @@ static char titlenormbgcolor[]           = "#222222";
 static char titlenormbordercolor[]       = "#444444";
 static char titlenormfloatcolor[]        = "#db8fd9";
 
-static char titleselfgcolor[]            = "#eeeeee";
+static char titleselfgcolor[]            = "#ef0404";
 static char titleselbgcolor[]            = "#df95b9";
 static char titleselbordercolor[]        = "#df95b9";
 static char titleselfloatcolor[]         = "#df95b9";
@@ -104,7 +105,7 @@ static char *colors[][ColCount] = {
 
 
 static const char *const autostart[] = {
-	"st", "wal -R", NULL,
+	"st", "wal -R", "st", NULL,
 	NULL /* terminate */
 };
 
@@ -178,7 +179,7 @@ static const Rule rules[] = {
 	RULE(.class = "discord", .tags = 1 << 2)
 	RULE(.class = "Spotify", .tags = 1 << 2)
 	RULE(.class = "thunderbird", .tags = 1 << 3)
-
+	RULE(.class = "KeePassXC", .tags = 1 << 4)
 
 
 };
@@ -207,7 +208,7 @@ static const BarRule barrules[] = {
 };
 
 /* layout(s) */
-static const float mfact     = 0.55; /* factor of master area size [0.05..0.95] */
+static const float mfact     = 0.5; /* factor of master area size [0.05..0.95] */
 static const int nmaster     = 1;    /* number of clients in master area */
 static const int resizehints = 0;    /* 1 means respect size hints in tiled resizals */
 static const int lockfullscreen = 1; /* 1 will force focus on the fullscreen window */
@@ -247,9 +248,17 @@ static const char *dmenucmd[] = {
 	"-sf", selfgcolor,
 	NULL
 };
+
 static const char *termcmd[]  = { "st", NULL };
-
-
+static const char *inclight[]  = { "xbacklight", "-inc", "10", NULL };
+static const char *declight[]  = { "xbacklight", "-dec", "10", NULL };
+static const char *upvol[]      = { "/usr/bin/wpctl",   "set-volume", "56",      "5%+",      NULL };
+static const char *downvol[]    = { "/usr/bin/wpctl",   "set-volume", "56",      "5%-",      NULL };
+static const char *mutevol[]    = { "/usr/bin/wpctl",   "set-mute",   "56",      "toggle",   NULL };
+static const char *medplaypausecmd[] = { "playerctl", "play-pause", NULL };
+static const char *mednextcmd[] = { "playerctl", "next", NULL };
+static const char *medprevcmd[] = { "playerctl", "previous", NULL };
+static const char *medstopcmd[] = { "playerctl", "stop", NULL };
 
 static const Key keys[] = {
 	/* modifier                     key            function                argument */
@@ -298,6 +307,15 @@ static const Key keys[] = {
 	{ MODKEY|ShiftMask,             XK_comma,      tagmon,                 {.i = -1 } },
 	{ MODKEY|ShiftMask,             XK_period,     tagmon,                 {.i = +1 } },
 	{ MODKEY,                       XK_n,          togglealttag,           {0} },
+	{ 0,                            XF86XK_MonBrightnessUp,     spawn,      {.v = inclight } },
+	{ 0,                            XF86XK_MonBrightnessDown,   spawn,      {.v = declight } },
+	{ 0,                       	XK_F2, spawn, {.v = downvol } },
+	{ 0,                       	XK_F1, spawn, {.v = mutevol } },
+	{ 0,                       	XK_F3, spawn, {.v = upvol   } },
+	{ 0, XF86XK_AudioPlay, spawn, {.v = medplaypausecmd } },
+	{ 0, XF86XK_AudioNext, spawn, {.v = mednextcmd } },
+	{ 0, XF86XK_AudioPrev, spawn, {.v = medprevcmd } },
+	{ 0, XF86XK_AudioStop, spawn, {.v = medstopcmd } },
 	TAGKEYS(                        XK_1,                                  0)
 	TAGKEYS(                        XK_2,                                  1)
 	TAGKEYS(                        XK_3,                                  2)
